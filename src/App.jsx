d4252974829b0a6da2515664f0326df4556fe385
Chidubemstanley <stanleyugwu444@gmail.com>
2023-11-25 18:04:15 +0100

    first commmit message

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import Navbar from './components/Navbar'
+import './App.css'
+import Hero from './components/Hero'
+import About from './components/About'
+import Support from './components/Support'
+import AlllinOne from './components/AlllinOne'
+import Pricing from './components/Pricing'
+import Footer from './components/Footer'
+
+
+function App() {
+ 
+
+  return (
+   <>
+   <Navbar />
+   <Hero />
+   <About />
+   <Support />
+   <AlllinOne />
+   <Pricing />
+   <Footer />
+
+   </>
+  );
+}
+
+export default App
---
+++ src/components/AlllinOne.jsx
@@ -0,0 +1,114 @@
+import React from 'react'
+import { CheckIcon } from '@heroicons/react/outline'
+
+const AlllinOne = () => {
+  return (
+    <div name='platforms' className='w-full my-32'>
+        <div className='max-w-[1240px] mx-auto px-2'>
+        <h2 className='text-5xl font-bold text-center  pt-6'>All-In-One Platform</h2>
+            <p className='text-2xl py-8 text-gray-500 text-center  pt-4'>Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Repellat doloribus sed, veritatis veniam corporis eos in doloremque hic at quia!
+            </p>
+            
+
+        <div className='grid sm:grid-cols-2 lg:grid-cols-4 gap-4'>
+
+         <div className='flex'>
+            <div>
+                <CheckIcon className='w-7 mr-4 text-green-600' />
+            </div>
+                <div>
+                    <h3 className='font-bold text-lg'>Notifications</h3>
+                    <p className='text-lg pt-2 pb-4'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla voluptatem
+                       id deleniti optio aliquam earum!</p>
+                </div>
+         </div>
+
+         <div className='flex'>
+            <div>
+                <CheckIcon className='w-7 mr-4 text-green-600' />
+            </div>
+                <div>
+                    <h3 className='font-bold text-lg'>Notifications</h3>
+                    <p className='text-lg pt-2 pb-4'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla voluptatem
+                       id deleniti optio aliquam earum!</p>
+                </div>
+         </div>
+
+         <div className='flex'>
+            <div>
+                <CheckIcon className='w-7 mr-4 text-green-600' />
+            </div>
+                <div>
+                    <h3 className='font-bold text-lg'>Notifications</h3>
+                    <p className='text-lg pt-2 pb-4'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla voluptatem
+                       id deleniti optio aliquam earum!</p>
+                </div>
+         </div>
+
+         <div className='flex'>
+            <div>
+                <CheckIcon className='w-7 mr-4 text-green-600' />
+            </div>
+                <div>
+                    <h3 className='font-bold text-lg'>Notifications</h3>
+                    <p className='text-lg pt-2 pb-4'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla voluptatem
+                       id deleniti optio aliquam earum!</p>
+                </div>
+         </div>
+
+         <div className='flex'>
+            <div>
+                <CheckIcon className='w-7 mr-4 text-green-600' />
+            </div>
+                <div>
+                    <h3 className='font-bold text-lg'>Notifications</h3>
+                    <p className='text-lg pt-2 pb-4'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla voluptatem
+                       id deleniti optio aliquam earum!</p>
+                </div>
+         </div>
+
+         <div className='flex'>
+            <div>
+                <CheckIcon className='w-7 mr-4 text-green-600' />
+            </div>
+                <div>
+                    <h3 className='font-bold text-lg'>Notifications</h3>
+                    <p className='text-lg pt-2 pb-4'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla voluptatem
+                       id deleniti optio aliquam earum!</p>
+                </div>
+         </div>
+
+         <div className='flex'>
+            <div>
+                <CheckIcon className='w-7 mr-4 text-green-600' />
+            </div>
+                <div>
+                    <h3 className='font-bold text-lg'>Notifications</h3>
+                    <p className='text-lg pt-2 pb-4'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla voluptatem
+                       id deleniti optio aliquam earum!</p>
+                </div>
+         </div>
+
+         <div className='flex'>
+            <div>
+                <CheckIcon className='w-7 mr-4 text-green-600' />
+            </div>
+                <div>
+                    <h3 className='font-bold text-lg'>Notifications</h3>
+                    <p className='text-lg pt-2 pb-4'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla voluptatem
+                       id deleniti optio aliquam earum!</p>
+                </div>
+         </div>
+
+    
+
+          </div>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default AlllinOne
